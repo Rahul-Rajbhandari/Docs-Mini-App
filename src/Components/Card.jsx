f0afa0eb65d74722801e1c744d3622f530b67183
Rahul <rahulrajbhandari19@gmail.com>
2024-08-23 22:42:53 +0545

    project started

---
+++ src/Components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { PiFilesBold } from "react-icons/pi";
+
+function Card() {
+  return (
+    <>
+        <div className="h-72 w-60 bg-zinc-900/90 rounded-[20px]">
+        <PiFilesBold/>
+        text
+        footer
+            data
+            tag
+
+        </div>
+    </>
+  )
+}
+
+export default Card
